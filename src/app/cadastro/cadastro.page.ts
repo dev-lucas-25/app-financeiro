23f9d871199898eff9aba9f738da6b0b6d303ad8
dev-lucas-25 <lucaspinheirorcsp@gmail.com>
2026-03-28 19:38:54 -0300

    alteração nas opções do select e ajuste de identação de funções

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-interface usuario{
-  nome:String,
-  tipo:String,
-  endereco:String
+interface usuario {
+  nome: String;
+  tipo: String;
+  endereco: String;
 }
 @Component({
   selector: 'app-cadastro',
@@ -12,39 +12,35 @@ interface usuario{
   styleUrls: ['./cadastro.page.scss'],
   standalone: false,
 })
-
-
 export class CadastroPage implements OnInit {
-  usuario= {
+  usuario = {
     nome: '',
     tipo: '',
-    endereco:'',
-  }
-  constructor(private router: Router) { }
-listaCadastro: usuario[] =[];
-  cadastrarUsuario(){
+    endereco: '',
+  };
+  constructor(private router: Router) {}
+  listaCadastro: usuario[] = [];
+  cadastrarUsuario() {
     const nome = this.usuario.nome.trim();
     const tipo = this.usuario.tipo;
     const endereco = this.usuario.endereco;
 
-    if(!nome || !tipo || !endereco){
+    if (!nome || !tipo || !endereco) {
       return;
     }
 
-    this.listaCadastro.unshift({nome, tipo,endereco});
+    this.listaCadastro.unshift({ nome, tipo, endereco });
     this.limparFormulario();
   }
-limparFormulario(){
- this.usuario ={
+  limparFormulario() {
+    this.usuario = {
       nome: '',
-      tipo:'',
-      endereco:''
-    }
-}
-excluirUsuario(index: number){
-this.listaCadastro.splice(index, 1)
-}
-  ngOnInit() {
+      tipo: '',
+      endereco: '',
+    };
   }
-
+  excluirUsuario(index: number) {
+    this.listaCadastro.splice(index, 1);
+  }
+  ngOnInit() {}
 }
