0f4771e55b48c4f59f3c0af8922701aff3cacd29
dev-lucas-25 <lucaspinheirorcsp@gmail.com>
2026-03-28 00:46:32 -0300

    adição de botões e configuração de rotas

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
@@ -8,8 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
+abrirCadastro(){
+    this.router.navigate(['/cadastro'])
+  }
+abrirPagar(){
+    this.router.navigate(['/pagar'])
+  }
+  abrirReceber(){
+    this.router.navigate(['/receber'])
+  }
   ngOnInit() {
   }
 
