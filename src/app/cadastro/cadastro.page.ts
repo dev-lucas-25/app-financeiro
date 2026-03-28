713a4ee3faa242887d093f7ea00663f092d1986b
dev-lucas-25 <lucaspinheirorcsp@gmail.com>
2026-03-28 18:46:01 -0300

    foram feito os design dos campos dos formularios e adicionado as logicas para cadastrar e visualizar os usuarios cadastrados

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+interface usuario{
+  nome:String,
+  tipo:String,
+  endereco:String
+}
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.page.html',
   styleUrls: ['./cadastro.page.scss'],
   standalone: false,
 })
+
+
 export class CadastroPage implements OnInit {
+  usuario= {
+    nome: '',
+    tipo: '',
+    endereco:'',
+  }
+  constructor(private router: Router) { }
+listaCadastro: usuario[] =[];
+  cadastrarUsuario(){
+    const nome = this.usuario.nome.trim();
+    const tipo = this.usuario.tipo;
+    const endereco = this.usuario.endereco;
 
-  constructor() { }
+    if(!nome || !tipo || !endereco){
+      return;
+    }
 
+    this.listaCadastro.unshift({nome, tipo,endereco});
+    this.limparFormulario();
+  }
+limparFormulario(){
+ this.usuario ={
+      nome: '',
+      tipo:'',
+      endereco:''
+    }
+}
+excluirUsuario(index: number){
+this.listaCadastro.splice(index, 1)
+}
   ngOnInit() {
   }
 
