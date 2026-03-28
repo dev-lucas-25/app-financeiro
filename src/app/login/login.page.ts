57a50319887d0932114046156d64de94a223c299
dev-lucas-25 <lucaspinheirorcsp@gmail.com>
2026-03-28 00:39:25 -0300

    criação de layout e configuração do botão de rota para o Menu

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -8,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  abrirMenu(){
+    this.router.navigate(['/menu'])
+  }
 
   ngOnInit() {
   }
