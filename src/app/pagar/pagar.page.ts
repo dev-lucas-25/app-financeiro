fc1d810c2b77239c87be89a7d206a9d3f0df4c4b
dev-lucas-25 <lucaspinheirorcsp@gmail.com>
2026-03-28 23:57:44 -0300

    Criação de campos e implementação da logica

--- src/app/pagar/pagar.page.ts
+++ src/app/pagar/pagar.page.ts
@@ -1,5 +1,13 @@
+import { NonNullAssert } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
+interface pagar{
+  fornecedor: String,
+  vencimento: String,
+  pagamento:String,
+  valor:Number,
+}
 @Component({
   selector: 'app-pagar',
   templateUrl: './pagar.page.html',
@@ -7,9 +15,36 @@ import { Router } from '@angular/router';
   standalone: false,
 })
 export class PagarPage implements OnInit {
-
+pagar={
+  fornecedor: '',
+  vencimento: '',
+  pagamento:'',
+  valor: null
+}
   constructor(private router: Router) { }
-
+  listaContasPagar: pagar[] = [];
+   cadastrarPagamento(){
+      const fornecedor = this.pagar.fornecedor.trim()
+      const vencimento = this.pagar.vencimento;
+      const pagamento = this.pagar.pagamento;
+      const valor = this.pagar.valor;
+  if (!fornecedor || !vencimento || !pagamento || valor == null || valor < 0) {
+      return;
+    }
+      this.listaContasPagar.unshift({fornecedor,vencimento,pagamento,valor});
+      this.limparCampos();
+    }
+  limparCampos(){
+this.pagar={
+  fornecedor: '',
+  vencimento: '',
+  pagamento:'',
+  valor: null 
+}
+  }
+  excluirContas(i:number){
+this.listaContasPagar.splice(i,1)
+  }
   ngOnInit() {
   }
 
